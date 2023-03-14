6962d65b575a552d9c0be8bd95236cb4b62e5d5e
aleix-abuli <aleixabuli088@gmail.com>
2023-03-14 11:03:45 +0100

    fixing public routes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,8 +6,8 @@
     <RouterLink to="/" class="nav-logo"><span>S</span>B<span>.</span></RouterLink>
     <p>Coded by Aleix Abulí.</p>
     <div class="footer-flex">
-      <a href="https://github.com/aleix-abuli" target="_blank"><img src="github.png" alt=""></a>
-      <a href="https://www.linkedin.com/in/aleix-abuli/" target="_blank"><img src="linkedin.png" alt=""></a>
+      <a href="https://github.com/aleix-abuli" target="_blank"><img src="/github.png" alt=""></a>
+      <a href="https://www.linkedin.com/in/aleix-abuli/" target="_blank"><img src="/linkedin.png" alt=""></a>
     </div>
   </footer>
 </template>
